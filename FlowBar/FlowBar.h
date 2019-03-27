6827cece0e8c0f654de8b41531627be3d442fa88
WillFletcher <wfletcher222@gmail.com>
2019-03-26 23:07:42 -0400

    Initial commit

---
+++ FlowBar/FlowBar.h
@@ -0,0 +1,19 @@
+//
+//  FlowBar.h
+//  FlowBar
+//
+//  Created by Will Fletcher on 3/26/19.
+//  Copyright © 2019 Will Fletcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowBar.
+FOUNDATION_EXPORT double FlowBarVersionNumber;
+
+//! Project version string for FlowBar.
+FOUNDATION_EXPORT const unsigned char FlowBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowBar/PublicHeader.h>
+
+
---
+++ FlowBarExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlowBarExamples
+//
+//  Created by Will Fletcher on 3/26/19.
+//  Copyright © 2019 Will Fletcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
